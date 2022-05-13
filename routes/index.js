936135f54f9f1f93aa248a1242c3cf108e1a5cd1
yeodahui <yeodahui.dev@gmail.com>
2022-05-13 16:30:32 +0900

    setting: html, css 초기환경 셋팅

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,9 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/", (req, res) => {
+  res.render("index", {postList: []});
+  // render로 index.ejs 보여줄거고 postList를 게시글에 대한 내용 보여줄거임
 });
 
 module.exports = router;
