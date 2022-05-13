88efbf0ff8727aef980fd7bf65ec6800477b56f3
yeodahui <yeodahui.dev@gmail.com>
2022-05-13 17:32:52 +0900

    add: auth, posts 이동 라우터 추가

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,14 @@
 var express = require("express");
 var router = express.Router();
+const authRouter = require("./auth");
+const postRouter = require("./posts");
 
 router.get("/", (req, res) => {
   res.render("index", {postList: []});
   // render로 index.ejs 보여줄거고 postList를 게시글에 대한 내용 보여줄거임
 });
+
+router.use("/auth", authRouter);
+router.use("/posts", postRouter);
 
 module.exports = router;
---
+++ routes/posts/index.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const router = express.Router();
+
+router.get("/upload", (req, res) => {
+  res.render("upload");
+});
+
+router.get("/:id", (req, res) => {
+  res.render("detail");
+});
+
+router.get("/update/:id", (req, res) => {
+  res.render("update");
+});
+
+module.exports = router;
